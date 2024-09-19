fd36a6c547e859733ea155e41c917811165db95f
r1nb0 <ilia.mesha44@gmail.com>
2024-09-20 00:18:04 +0300

    add postgres connection

--- auth-svc/pkg/postgres/postgres.go
+++ auth-svc/pkg/postgres/postgres.go
@@ -1,1 +1,35 @@
 package postgres
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"github.com/r1nb0/food-app/auth-svc/internal/config"
+)
+
+func InitDB(cfg *config.Config) (*pgx.Conn, error) {
+	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		cfg.Postgres.Username,
+		cfg.Postgres.Password,
+		cfg.Postgres.Host,
+		cfg.Postgres.Port,
+		cfg.Postgres.DBName,
+		cfg.Postgres.SSLMode,
+	)
+
+	conn, err := pgx.Connect(context.Background(), url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = conn.Ping(context.Background()); err != nil {
+		return nil, err
+	}
+
+	return conn, err
+}
+
+func CloseDB(conn *pgx.Conn) error {
+	return conn.Close(context.Background())
+}
