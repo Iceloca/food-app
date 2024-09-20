81703b6406a92db23dac70ceaa4f087d65990384
r1nb0 <ilia.mesha44@gmail.com>
2024-09-20 23:33:04 +0300

    impl run logic

--- auth-svc/cmd/main.go
+++ auth-svc/cmd/main.go
@@ -3,11 +3,32 @@ package main
 import (
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/r1nb0/food-app/auth-svc/internal/app"
+	"github.com/r1nb0/food-app/auth-svc/internal/config"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	cfg := config.GetConfig()
+
+	application := app.New(cfg)
+
+	go func() {
+		application.GRPCServer.Run()
+	}()
+
+	stop := make(chan os.Signal, 1)
+
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	<-stop
+
+	application.GRPCServer.Stop()
 }
