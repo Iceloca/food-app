3de5c5fff32ed9e4e67b9dd0c71b56a7823e860d
r1nb0 <ilia.mesha44@gmail.com>
2024-09-20 21:22:27 +0300

    impl jwt-token creation

--- auth-svc/internal/lib/jwt.go
+++ auth-svc/internal/lib/jwt.go
@@ -1,1 +1,24 @@
 package lib
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/r1nb0/food-app/auth-svc/internal/domain/models"
+	"os"
+	"time"
+)
+
+func NewToken(user models.User, duration time.Duration) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+	claims["uid"] = user.ID
+	claims["email"] = user.Email
+	claims["exp"] = time.Now().Add(duration).Unix()
+
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
