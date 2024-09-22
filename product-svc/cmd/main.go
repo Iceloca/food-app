a13c5cfdf6b34cc5f390fbabeaef7fa80ad947e6
r1nb0 <ilia.mesha44@gmail.com>
2024-09-22 19:47:00 +0300

    add init mongo client

--- product-svc/cmd/main.go
+++ product-svc/cmd/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"github.com/joho/godotenv"
-	"github.com/r1nb0/food-app/product-svc/internal/cfg"
 	"log"
 )
 
@@ -11,5 +9,4 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	fmt.Println(cfg.GetConfig())
 }
--- product-svc/internal/lib/mongo/mongo.go
+++ product-svc/internal/lib/mongo/mongo.go
@@ -1,1 +1,27 @@
 package mongo
+
+import (
+	"context"
+	"fmt"
+	"github.com/r1nb0/food-app/product-svc/internal/config"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"time"
+)
+
+func InitClient(cfg *config.Config) (*mongo.Client, error) {
+	url := fmt.Sprintf("mongodb://%s:%s", cfg.MongoDB.Host, cfg.MongoDB.Port)
+
+	client, err := mongo.Connect(options.Client().ApplyURI(url))
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err = client.Ping(ctx, nil); err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
