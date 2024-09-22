f6cfa742d02e39de8e1e534a99243a56e1017f96
r1nb0 <ilia.mesha44@gmail.com>
2024-09-23 00:38:45 +0300

    change mongo-driver version

--- product-svc/internal/lib/mongo/mongo.go
+++ product-svc/internal/lib/mongo/mongo.go
@@ -4,20 +4,22 @@ import (
 	"context"
 	"fmt"
 	"github.com/r1nb0/food-app/product-svc/internal/config"
-	"go.mongodb.org/mongo-driver/v2/mongo"
-	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"time"
 )
 
 func InitClient(cfg *config.Config) (*mongo.Client, error) {
 	url := fmt.Sprintf("mongodb://%s:%s", cfg.MongoDB.Host, cfg.MongoDB.Port)
 
-	client, err := mongo.Connect(options.Client().ApplyURI(url))
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(url))
 	if err != nil {
 		return nil, err
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	if err = client.Ping(ctx, nil); err != nil {
 		return nil, err
