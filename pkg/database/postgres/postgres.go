3f4eb55469f2e5c48886aaefa1d5aeb89c684e59
r1nb0 <ilia.mesha44@gmail.com>
2024-10-06 21:53:57 +0300

    impl logger interface & logrus

--- pkg/database/postgres/postgres.go
+++ pkg/database/postgres/postgres.go
@@ -1,1 +1,19 @@
 package database
+
+import (
+	"github.com/jmoiron/sqlx"
+)
+
+func InitPostgres(dataSourceName string) (*sqlx.DB, error) {
+	db, err := sqlx.Connect("postgres", dataSourceName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, err
+}
