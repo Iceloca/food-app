1f91b6a80efe0ffc0175a08ea11825b3ff78b712
r1nb0 <ilia.mesha44@gmail.com>
2024-10-08 23:17:59 +0300

    impl init mongo client

--- pkg/database/mongodb/mongodb.go
+++ pkg/database/mongodb/mongodb.go
@@ -1,5 +1,25 @@
-package mongo
+package mongodb
 
-func InitDB(url string) {
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"time"
+)
 
+func InitClient(uri string) (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		return nil, err
+	}
+
+	return client, nil
 }
--- pkg/go.mod
+++ pkg/go.mod
@@ -5,5 +5,6 @@ go 1.23.1
 require (
 	github.com/jmoiron/sqlx v1.4.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	go.mongodb.org/mongo-driver v1.17.1 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 )
